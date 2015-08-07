61df4fa1f4e53de8dadf661ea9b039a4288fef98
(EXT) Francesco Pischedda <francesco.pischedda@gmail.com>
2015-08-07 11:14:15 +0200

    update boundaries using keyboard

--- example/game_scene.py
+++ example/game_scene.py
@@ -23,19 +23,32 @@ class GameScene(Scene):
 
         image = pyglet.resource.image('res/images/sprites/sprite.png')
 
-        boundaries = (0, 0, 640, 480)
+        self.boundaries = [0, 0, 640, 480]
 
         self.batch = pyglet.graphics.Batch()
-        self.balls = [randomize_ball(boundaries, image, self.batch)
+        self.balls = [randomize_ball(self.boundaries, image, self.batch)
                       for _ in range(1000)]
 
         self.background = pyglet.resource.image('res/images/bg/bg.jpg')
 
     def on_key_press(self, symbol, modifier):
+        if symbol == pyglet.window.key.M:
+            self.boundaries[0] += 10
+            self.boundaries[1] += 10
+            self.boundaries[2] -= 10
+            self.boundaries[3] -= 10
+
+        if symbol == pyglet.window.key.N:
+            self.boundaries[0] -= 10
+            self.boundaries[1] -= 10
+            self.boundaries[2] += 10
+            self.boundaries[3] += 10
+
         return True
 
     def on_update(self, dt):
-        [b.on_update(dt) for b in self.balls]
+        for b in self.balls:
+            b.on_update(dt)
 
     def on_draw(self, window):
 
