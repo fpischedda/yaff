103361ccad932cce8528224942575bdee4a1fc30
(EXT) Francesco Pischedda <francesco.pischedda@gmail.com>
2015-10-11 12:08:38 +0200

    added to_maximum and to_minimum methods to gain/decay mixin with tests

--- crazy_cat_couple/lib/property.py
+++ crazy_cat_couple/lib/property.py
@@ -3,39 +3,49 @@ class BaseProperty:
 
     def __init__(self, name, value, *args, **kwargs):
         super(BaseProperty, self).__init__(*args, **kwargs)
-        
+
         self.value = value
         self.name = name
+
 
 class DecayingPropertyMixin:
 
     DECAYING_ATTIBUTE = 'value'
-    
+
     def __init__(self, decay_by_sec, min_value, *args, **kwargs):
 
         super(DecayingPropertyMixin, self).__init__(*args, **kwargs)
-        
+
         self.decay_by_sec = decay_by_sec
         self.min_value = min_value
 
+    def to_minimum(self):
+        setattr(self, self.DECAYING_ATTIBUTE, self.min_value)
+        return getattr(self, self.DECAYING_ATTIBUTE)
+
     def decay(self, dt):
         val = getattr(self, self.DECAYING_ATTIBUTE)
-        
+
         new_val = max(val - self.decay_by_sec * dt, self.min_value)
 
         setattr(self, self.DECAYING_ATTIBUTE, new_val)
         return new_val
 
+
 class GainingPropertyMixin:
 
     GAINING_ATTRIBUTE = 'value'
-    
+
     def __init__(self, gain_by_sec, max_value, *args, **kwargs):
 
         super(GainingPropertyMixin, self).__init__(*args, **kwargs)
 
         self.gain_by_sec = gain_by_sec
         self.max_value = max_value
+
+    def to_maximum(self):
+        setattr(self, self.DECAYING_ATTIBUTE, self.max_value)
+        return getattr(self, self.DECAYING_ATTIBUTE)
 
     def gain(self, dt):
         val = getattr(self, self.GAINING_ATTRIBUTE)
--- crazy_cat_couple/tests/test_property.py
+++ crazy_cat_couple/tests/test_property.py
@@ -1,5 +1,6 @@
 from lib.property import DynamicProperty
 
+
 def test_property_decay():
 
     p = DynamicProperty(name='test',
@@ -10,6 +11,7 @@ def test_property_decay():
                         gain_by_sec=1)
 
     assert p.decay(1) == 0
+
 
 def test_property_gain():
 
@@ -22,6 +24,7 @@ def test_property_gain():
 
     assert p.gain(1) == 2
 
+
 def test_property_reset():
 
     p = DynamicProperty(name='test',
@@ -33,3 +36,27 @@ def test_property_reset():
 
     p.decay(0.2)
     assert p.reset() == 1
+
+
+def test_to_minimum():
+
+    p = DynamicProperty(name='test',
+                        initial_value=1,
+                        min_value=0,
+                        max_value=2,
+                        decay_by_sec=1,
+                        gain_by_sec=1)
+
+    assert p.to_minimum() == 0
+
+
+def test_to_maximum():
+
+    p = DynamicProperty(name='test',
+                        initial_value=1,
+                        min_value=0,
+                        max_value=2,
+                        decay_by_sec=1,
+                        gain_by_sec=1)
+
+    assert p.to_maximum() == 2
