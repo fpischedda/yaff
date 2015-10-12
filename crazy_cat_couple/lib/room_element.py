5ffda4b83985554e438e3de66e2444e2bcd7f946
(EXT) Francesco Pischedda <francesco.pischedda@gmail.com>
2015-10-12 19:31:44 +0200

    added cleaness property to room element

--- crazy_cat_couple/lib/room_element.py
+++ crazy_cat_couple/lib/room_element.py
@@ -1,3 +1,6 @@
+from property import DynamicProperty
+
+
 class RoomElement:
 
     DEFAULT_OPTIONS = {
@@ -9,6 +12,13 @@ class RoomElement:
 
         opts = dict(RoomElement.DEFAULT_OPTIONS, **options)
         self.maintenance_cost = opts['maintenance_cost']
+
+        self.cleannes = DynamicProperty(name='cleannes',
+                                        initial_value=100,
+                                        min_value=0,
+                                        max_value=100,
+                                        decay_by_sec=0.001,
+                                        gain_by_sec=0.1)
 
     def draw(self, window):
         pass
