5093a940d9a3d587e78c9aafe3719a057662da4e
(EXT) Francesco Pischedda <francesco.pischedda@gmail.com>
2015-11-05 11:46:33 +0100

    load forniture from json

--- crazy_cat_couple/game.py
+++ crazy_cat_couple/game.py
@@ -1,10 +1,27 @@
+import json
+import pyglet.resource
 from lib.room import Room
 from lib.forniture import Forniture
+
+
+def load_from_json(filename):
+    """load Forniture definition form a json file"""
+
+    with pyglet.resource.file(filename, 'rt') as file:
+
+        definitions = json.loads(file.read())
+
+        forniture = {item["name"]: Forniture(**item)
+                     for item in definitions["definitions"]}
+
+    return forniture
 
 
 class Game:
 
     def __init__(self):
+
+        self.forniture = load_from_json("res/forniture/forniture.json")
 
         self.rooms = [
             Room('living room', 20, 10),
@@ -12,9 +29,7 @@ class Game:
             Room('bedroom', 15, 8)
         ]
 
-        forniture = Forniture("window", 50, 50, 0)
-
-        self.rooms[0].add_forniture(forniture, 10, 100)
+        self.rooms[0].add_forniture(self.forniture['window'], 10, 100)
 
         self.persons = []
 
