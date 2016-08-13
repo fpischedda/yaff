815a766224493e48f4327e9e2bd1be3dc00f6ee8
e_fpischedda <francesco.pischedda@abinsula.com>
2016-08-13 16:55:07 +0200

    added sound and sound loop to SplashScene builtin scene

--- example_bouncing_balls/main.py
+++ example_bouncing_balls/main.py
@@ -4,4 +4,6 @@ from .game_scene import GameScene
 
 
 if __name__ == '__main__':
-    app.run(SplashScene(next_scene_class=GameScene))
+    app.run(SplashScene(next_scene_class=GameScene,
+                        sound_file_path='res/sfx/example_loop.wav',
+                        sound_loop=True))
