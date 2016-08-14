975d35cb3fd1a4e9c90a869e66737d7e0d982abb
fpischedda <francesco.pischedda@gmail.com>
2016-08-14 13:27:39 +0100

    new splash screen and 'terrifing' sound loop

--- horror/main.py
+++ horror/main.py
@@ -4,4 +4,7 @@ from .game_scene import GameScene
 
 
 if __name__ == '__main__':
-    app.run(SplashScene(next_scene_class=GameScene))
+    app.run(SplashScene(next_scene_class=GameScene,
+                        splash_image_path='res/images/splash.png',
+                        sound_file_path='res/sfx/girl_cry.ogg',
+                        sound_loop=True))
